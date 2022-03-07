186b7d60aebc85942ff20e98d0e458ec882e88b8
kickeyhickey <hickeydeveloper@gmail.com>
2022-03-06 17:02:28 -0800

    initial setup complete

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,9 +1,47 @@
 import React, { Component } from 'react'
+import About from './pages/About'
+import Home from './pages/Home'
+import ApartmentIndex from './pages/ApartmentIndex'
+import ApartmentShow from './pages/ApartmentShow'
+import ApartmentNew from './pages/ApartmentNew'
+import mockData from './mockData'
+import {
+  BrowserRouter as  Router,
+  Route,
+  Switch
+} from 'react-router-dom'
 
-export default class App extends Component {
+
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+      this.state = {
+        apartments: mockData
+      }
+  }
   render() {
-    return (
-    <h1>hello world</h1>
+    const {
+      logged_in,
+      current_user,
+      new_user_route,
+      sign_in_route,
+      sign_out_route
+    } = this.props
+    return(
+      <>
+       <Router>
+        <Switch>
+          <Route exact path="/" component={Home} /> 
+          <Route exact path="/apartmentindex" component={ApartmentIndex} />
+          <Route exact path="/apartmentnew" component={ApartmentNew} />
+          <Route exact path="/apartmentshow" component={ApartmentShow} />
+         <Route path="/about" component={About} /> 
+        </Switch>
+      </Router>
+      </>
     )
   }
 }
+
+export default App
