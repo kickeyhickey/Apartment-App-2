0b54723b6bdd4418bfa9c2834498673ccb65253a
kickeyhickey <hickeydeveloper@gmail.com>
2022-04-03 21:28:08 -0700

    additional styling added

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import ApartmentIndex from './ApartmentIndex'
 
 class Home extends Component {
   render() {
     return(
+      <>
       <h3>This is the Home Page</h3>
+      </>
     )
   }
 }
